4649932658509570882338384af748073d6adfca
nabl95 <touatnabil2005@gmail.com>
2025-04-18 18:43:30 +0200

    code #2
    Ajout, changement, perfectionnement de fonction. De plus On a décider de créer un seul fichier donc on a du changer le code.

--- fonction.c
+++ fonction.c
@@ -3,11 +3,10 @@
 #include "string.h"
 
 
-void lire_un_fichier(char *fic)
+void lire_aff_un_fichier(char *fic)
 {
     FILE * fichier_lire=fopen(fic,"r");
     char chaine[256];
-    int acc=0;
     // ajout d une fct qui eneleve le txt du fichier pour le printf lige 22
 
     if (fichier_lire == NULL) {
@@ -17,34 +16,68 @@ void lire_un_fichier(char *fic)
     
     while((fgets(chaine,255,fichier_lire)) != NULL)
     {
-        acc++;
         printf("%s",chaine);
     } printf("\n");
-    
-    printf("il y a %d \n",acc);
     fclose(fichier_lire);
-
 }
 
-void ajout_animal(char *fic)
+int compter_nb_animal(char *fic)
+{
+    FILE * fichier_lire=fopen(fic,"r");
+    char chaine[256];
+    int acc=0;
+
+    if (fichier_lire == NULL) {
+        printf("erreur de fichier\n");
+        exit(1);
+     }
+     
+    while((fgets(chaine,(sizeof(chaine)-1),fichier_lire)) != NULL)
+    {
+        acc++;
+    }
+    fclose(fichier_lire);
+    return acc;
+
+   
+
+
+}
+void ajout_animal(char *fic)//ajout commentaire pour animal
 {
     FILE * fichier_w=fopen(fic,"a");
-
     char mot[230];
-    int tl;
-    printf("donner le nom du chat:\n");
-    scanf("%s",mot);
-    fputs(mot,fichier_w);
+    int tl,id;
+    char nom[100],espece[100];
+
+    if(!fichier_w)
+    {
+        perror("Erreur d'ouverture\n");
+        exit(33);
+    }
+
+    if(compter_nb_animal(fichier_w)>=50)
+    {
+        printf("Impossible,trop d'animaux!\n");
+        return;
+    }
+
+    printf("Donner son nom son espece et son age svp\n");
+    scanf("%s %s %d",nom,espece,&age);
+    id=rand()%3001;
+
+    fprintf(fichier_w,"%s %s %d %d",nom,espece,age,id);
+    printf("L'animal suivant a été ajouté,Voici ces informations:\n%s %s %d %d",nom,espece,age,id);
     fclose(fichier_w);
 }
 
-void rechercher_animal(char *fic,char nom, int age)
+void rechercher_animal(char *fic)//char nom, int age)?
 {
     int age,id,flag;
-    char nom[256];
+    char nom[256],espece[256];
     int age_voullu;
     int type_age;
-    char nom_voullu[256];
+    char nom_voullu[256],espece_voullu[256];
 
     FILE *f_lire=fopen(fic,"r");
     if (f_lire == NULL) {
@@ -54,8 +87,8 @@ void rechercher_animal(char *fic,char nom, int age)
 
     do{
 
-    printf("Donner son nom\n");
-    scanf("%s",nom_voullu);
+    printf("Donner son nom,son espece\n");
+    scanf("%s %s",nom_voullu,espece_voullu);
     printf("On passe a l'age\n");
     printf("Voulez vous un animal 1:jeune(<2ans)\n 2:sénior(>10ans)\n");
     scanf("%d",&type_age);
@@ -74,7 +107,7 @@ void rechercher_animal(char *fic,char nom, int age)
             exit(3);
         }
     }
-    if(type_age==1)
+    if(type_age==2)
     {
         printf("donner l'age voullu\n");
         scanf("%d",&age_voullu);
@@ -85,12 +118,12 @@ void rechercher_animal(char *fic,char nom, int age)
         }
     }
 
-    while(fscanf(f_lire,%s %d %d,nom,&age,&id)==3)
+    while(fscanf(f_lire,%s %s %d %d,nom,espece,&age,&id)==3)
     {
-        if(age==age_voullu && (strcmp(nom,nom_voullu)==0))
+        if((age==age_voullu) && (strcmp(nom,nom_voullu)==0) && (strcmp(espece,espece_voullu)==0))
         {
-            printf("Super!!\nCette animal existe\n");
-            printf("%s %d %d",nom,age,id);
+            printf("Super!!\nCette animal existe :\n");
+            printf("%s %s %d %d",nom,espece,age,id);
         }
         
         else
@@ -112,11 +145,12 @@ void rechercher_animal(char *fic,char nom, int age)
 void adoption(Char *fic)
 {
 
+
 }
 int main(){
 
     char *b="chat.txt";
-    lire_un_fichier(b);
+    fct(b);
 
 
    
