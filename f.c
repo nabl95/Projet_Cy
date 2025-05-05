d70fbe41e5ec0997117656edee4b42642e220cf2
nabl95 <touatnabil2005@gmail.com>
2025-05-05 19:19:38 +0200

    code #6

--- f.c
+++ f.c
@@ -5,6 +5,7 @@
 #include "ctype.h"
 #define TAILLE_MAX 150
 #define MAX_ANIMAL 50
+#define MAX_CHAINE 256
 #define YEAR    2025
 typedef struct{
 
@@ -58,22 +59,18 @@ int en_lettre(char * chaine)
     return 1;
 }
 
-char * lettre_minuscule(char * caract)
+void lettre_minuscule(char * caract, char *chaine)
 {
-    char *chaine;
     int taille;
-    chaine=malloc(255*sizeof(char));
     do{
         printf("donner votre %s",caract);
-        fgets(chaine,255,stdin);//on utlise sa car le scanf s art si il y a un espace
+        fgets(chaine,MAX_CHAINE,stdin);//on utlise sa car le scanf s art si il y a un espace
         taille = strlen(chaine);
         if (taille> 0 && chaine[taille- 1] == '\n') {//permet d enlever le '\n' si il existe
             chaine[taille- 1] = '\0';//explique ca 
         }
-        en_lettre(chaine);
     } while(en_lettre(chaine)!=1);
     en_minuscule(chaine);
-    return chaine;//free() ?
     
 }
 int est_entier(char *caract)
@@ -83,10 +80,10 @@ int est_entier(char *caract)
     do{
         printf("Donner son %s\n",caract);
         verif_s=scanf("%d",&nb);
+        verif_scanf_entier();
         if(verif_s !=1 )
         {
             printf("Erreur !!\n Rentrer un entier valide\n");
-            verif_scanf_entier();
         }
     }while(verif_s!=1);
     return nb;
@@ -97,10 +94,11 @@ int est_entier_choix(int min,int max)
     do{
         printf("Selectionner : \n %d : OUI\n %d : NON\n",min,max);
         verif_s=scanf("%d",&choix);
+        verif_scanf_entier();
         if(verif_s !=1 )
         {
             printf("Erreur !!\n Rentrer un entier valide\n");
-            verif_scanf_entier();
+            
         }
         else if( choix != min && choix != max)// else if car sa peut etre un entier mais pas compris entre les deux valeur impose
         {
@@ -116,25 +114,25 @@ float est_flottant(char *caract)
     do{
         printf("Donner son %s\n",caract);
         verif_s=scanf("%f",&nb);
+        verif_scanf_flottant();
         if(verif_s !=1 )
         {
             printf("Erreur !!\n Rentrer un floattant valide\n");
-            verif_scanf_flottant();
         }
     }while(verif_s!=1);
     return nb;
 }
-float est_flottant_choix(int min,int max)
-{
+float est_flottant_choix(int min,int max){
    float choix;
    int verif_s;
     do{
         printf("Selectionner : \n %d : OUI\n %d : NON\n",min,max);
         verif_s=scanf("%f",&choix);
+        verif_scanf_flottant();
         if(verif_s !=1 )
         {
             printf("Erreur !!\n Rentrer un flottant valide\n");
-            verif_scanf_flottant();
+           
         }
         else if( choix != min && choix != max)// else if car sa peut etre un float mais pas compris entre les deux valeur impose
         {
@@ -207,23 +205,23 @@ void aff(Animal t_animaux[],int *nb_animaux)
 
 int fic_to_struct(Animal tab_animaux[])
 {//fonction de depart pour mettre tous les animaux du fichier dans la tabl struct
-    char ligne[256],espece[256],nom[256],commentaire[256];
+    char ligne[MAX_CHAINE],espece[MAX_CHAINE],nom[MAX_CHAINE],commentaire[MAX_CHAINE];
     int nb_animaux,n,id,annee,i=0;
     float poids;
 
     FILE *fic=fopen("animal.txt","r");
     if(fic == NULL)
     {
-        printf("Erreur d'ouverture du fichierhhhh\n");
+        printf("Erreur d'ouverture du fichier\n");
         exit(1);
     }
     if (fscanf(fic, "%d\n", &nb_animaux) != 1) {
-        printf("❌ Erreur : première ligne invalide.\n");
+        printf("Erreur : première ligne invalide.\n");
         fclose(fic);
         exit(1);
     } 
 
-    while (fgets(ligne, sizeof(ligne)-1, fic) != NULL && i<MAX_ANIMAL)
+    while (fgets(ligne, MAX_CHAINE, fic) != NULL && i< MAX_ANIMAL)
     {
         n = sscanf(ligne, "%d %s %s %d %f %s",&id, nom, espece, &annee, &poids, commentaire);
 
@@ -256,22 +254,36 @@ void ajt_animal_strt(Animal t_animaux[],int *nb_animaux)
     Animal a;
     int verif_s;
     int choix_c;
+    if(*nb_animaux>=50)
+    {
+        printf("Le refuge est deja plein\n");
+    }
+    else{
     printf("\n--- Ajouter un nouvel animal ---\n");
 
-    strcpy(a.nom,lettre_minuscule("nom "));
-    strcpy(a.espece,lettre_minuscule("espece "));
+    lettre_minuscule("nom ",a.nom);
+    lettre_minuscule("espece ",a.espece);
 
-    a.annee=est_entier("annee_de_naissance ");
+    do
+    {
+        a.annee=est_entier("annee_de_naissance ");
+        if(a.annee>=YEAR)
+        {
+            printf("Annee invalide\n");
+        }
+    }while(a.annee>=YEAR);
     a.poids=est_flottant("poids ");
     a.id=id_unique(t_animaux,nb_animaux);
+    printf("Ajoutez un commentaire\n");
     choix_c=est_entier_choix(0,1);// 0=oui     1=Non
     if(choix_c == 0)
     {
-        strcpy(a.commentaire,lettre_minuscule("commentaire "));
+        lettre_minuscule("commentaire ",a.commentaire);
     }
     
     t_animaux[*nb_animaux] = a;
     (*nb_animaux)++;  // ou (*nb_animaux) ????
+}
 }
 void ajt_animal_fic(Animal t_animaux[],int *nb_animaux)
 {//permet d'ajouter tous les animaux dans le fichier
@@ -282,7 +294,7 @@ void ajt_animal_fic(Animal t_animaux[],int *nb_animaux)
         printf("Erreur d'ouverture du fichier\n");
         return;
     }
-    fprintf(fichier, "%d\n", nb_animaux);
+    fprintf(fichier, "%d\n", *nb_animaux);
 
     for(int i=0;i<*nb_animaux;i++)//attention si pas de commentaire a modifier
     {
@@ -291,17 +303,19 @@ void ajt_animal_fic(Animal t_animaux[],int *nb_animaux)
         {
             fprintf(fichier, " %s\n",t_animaux[i].commentaire);
         }
+        else fprintf(fichier,"\n");
     }
 fclose(fichier);
 }
 void recherche_animal(Animal t_animaux[],int *nb_animaux)
 {
-    char nom[256],espece[256];
+    char nom[MAX_CHAINE],espece[MAX_CHAINE];
     int age,choix_a,find=0,flag;
     do{
         printf("\n--- RECHERCHER UN ANIMAL ---\n");
-        strcpy(nom,lettre_minuscule("nom "));
-        strcpy(espece,lettre_minuscule("espece "));
+        lettre_minuscule("nom ",nom);
+        lettre_minuscule("espece ",espece);
+        printf("selectionner son type d'age\n 0 : jeune  1 : senior\n");
         choix_a=est_entier_choix(0,1);//0= age<2      1= age>10
     
         if(choix_a==0)
@@ -349,7 +363,7 @@ void recherche_animal(Animal t_animaux[],int *nb_animaux)
 
 void adoption(Animal t_animaux[],int *nb_animaux)
 {
-    int id,choix,find,z=0;
+    int id,choix,find=0,z=0;
     Animal temp_animaux[50];
 
     aff(t_animaux,nb_animaux);
@@ -368,10 +382,12 @@ void adoption(Animal t_animaux[],int *nb_animaux)
             if(choix == 0)
             {
                 printf("Felicitation %s est a vous!\n Prenez bien soin de lui \n",t_animaux[i].nom);
-                continue;//Pas ajouter dans tabl temp
+                //continue;//Pas ajouter dans tabl temp
             }
             else{
                 printf("Adoption annule\n");
+                temp_animaux[z]=t_animaux[i];//important car on ne rentrera pas dans le 2eme else si on decide de na pas adopter finalement 
+                z++;
             }
         }
         else{
@@ -397,22 +413,24 @@ void adoption(Animal t_animaux[],int *nb_animaux)
 void day_clean(Animal t_animaux[],int *nb_animaux)
 {
     
-    int charge_tr,quotidien=0,hebdo=0,t_minute=0;
+    int charge_tr,quotidien=0,hebdo=0,total_minute=0;
     printf("\n========= CHARGE DE NETTOYAGE HEBDOMADAIRE =========\n");
  
     for(int i=0;i<*nb_animaux;i++)
     {
+        quotidien = 0;
+        hebdo = 0;
         if(strcmp(t_animaux[i].espece,"hamster") == 0 ||  strcmp(t_animaux[i].espece, "chat") == 0)
         {
             quotidien =10;
             hebdo =20;
         }
-        else if(strcmp(t_animaux[i].espece,"autruche"))
+        else if(strcmp(t_animaux[i].espece,"autruche") == 0)
         {
             quotidien =20;
             hebdo =45;
         }
-        else if(strcmp(t_animaux[i].espece,"chien"))
+        else if(strcmp(t_animaux[i].espece,"chien") == 0)//QUESTION
         {
             quotidien =5;
             hebdo = 20;
@@ -421,24 +439,71 @@ void day_clean(Animal t_animaux[],int *nb_animaux)
             quotidien=2;
             hebdo=0;
         }
-    charge_tr=(quotidien*7)+hebdo;
-    t_minute+=charge_tr;
+    charge_tr=(quotidien*7)+hebdo;//charge par animal
+    total_minute+=charge_tr;//incrementation du temps total
     printf("%-12s (%-8s) : %2d min/jour + %2d min/sem = %3d min/sem\n",t_animaux[i].nom,t_animaux[i].espece,quotidien,hebdo,charge_tr);
 }
 
 printf("-----------------------------------------------------\n");
-printf(" Temps total de nettoyage hebdomadaire : %d heures et %d minutes\n",t_minute / 60, t_minute % 60);
+printf(" Temps total de nettoyage hebdomadaire : %d heures et %d minutes\n",total_minute / 60, total_minute % 60);
+}
+
+void menu(Animal t_animaux[], int *nb_animaux)
+{
+    int choix;
+
+    do {
+        printf("\n=========== MENU DU REFUGE ===========\n");
+        printf("1. Afficher tous les animaux\n");
+        printf("2. Ajouter un nouvel animal\n");
+        printf("3. Enregistrer les animaux dans le fichier\n");
+        printf("4. Rechercher un animal\n");
+        printf("5. Adopter un animal\n");
+        printf("6. Voir la charge de nettoyage\n");
+        printf("0. Quitter\n");
+        printf("======================================\n");
+
+        choix = est_entier("choix");  
+
+        switch (choix)
+        {
+            case 1:
+                aff(t_animaux, nb_animaux);
+                break;
+            case 2:
+                ajt_animal_strt(t_animaux, nb_animaux);
+                ajt_animal_fic(t_animaux, nb_animaux);
+                break;
+            case 3:
+                ajt_animal_fic(t_animaux, nb_animaux);
+                break;
+            case 4:
+                recherche_animal(t_animaux, nb_animaux);
+                break;
+            case 5:
+                adoption(t_animaux, nb_animaux);
+                ajt_animal_fic(t_animaux, nb_animaux);
+                break;
+            case 6:
+                day_clean(t_animaux, nb_animaux);
+                break;
+            case 0:
+                printf("A bientot !\n");
+                break;
+            default:
+                printf("Choix invalide. Veuillez réessayer.\n");
+        }
+    } while (choix != 0);
 }
 int main(){
     srand(time(NULL));
-    Animal t_animaux[50];
+    Animal t_animaux[MAX_ANIMAL ];
     int nb_animaux=0;
     nb_animaux=fic_to_struct(t_animaux);
-    aff(t_animaux, &nb_animaux);
-    day_clean(t_animaux, &nb_animaux);
+    menu(t_animaux,&nb_animaux);
 }
 
 
-
-
-
+//question sur l age entre 2  et 10. on doit rechercher selon quel critere
+// do while dans l adoption
+// enelevr le choix 3 dans menu 
