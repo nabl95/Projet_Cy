7e6ed5d7305bdb13f71a89aab56ccb39b40f74fa
nabl95 <touatnabil2005@gmail.com>
2025-05-10 22:33:37 +0200

    Update main.c

--- projet_info/main.c
+++ projet_info/main.c
@@ -1,25 +1,45 @@
 #include "refuge.h"
 
 void afficher_menu() {
-  printf("\n=== MENU DU REFUGE ===\n");
-  printf("1. Ajouter un animal\n");
-  printf("2. Afficher les animaux\n");
-  printf("3. Rechercher un animal\n");
-  printf("4. Adopter un animal\n");
-  printf("5. Calculer la charge de nettoyage\n");
-  printf("0. Quitter\n");
-  printf("Choisissez une option : ");
+  printf("\n");
+        printf("============================================================\n");
+        printf("                MENU PRINCIPAL - LE REFUGE ANIMALIER          \n");
+        printf("============================================================\n\n");
+
+        printf("    1. AFFICHER TOUS LES ANIMAUX\n");
+        printf("       -> Vous etre peut-etre a la recherche de votre prochaine animal\n\tOn vous propose de les voir virtuellement\n\n");
+
+        printf("    2. AJOUTER UN NOUVEL ANIMAL\n");
+        printf("       -> Vous venez de trouver un animal ? Ajoute le chez nous.\n\n");
+
+        printf("    3. RECHERCHER UN ANIMAL\n");
+        printf("       -> Vous avez perdu votre animal ou souhaitez vous renseigner sur un animal de notre refuge ?\n\tRecherche le !\n\n");
+
+        printf("    4. ADOPTER UN ANIMAL\n");
+        printf("       -> Vous avez eu un coup de coeur ? Passer a la vitesse superieur en l'adoptant\n\n");
+
+        printf("    5. VOIR LA CHARGE DE NETTOYAGE\n");
+        printf("       -> Un animal vous interrese et vous souhaitez vous renseigner sur son temps de netoyage ?\n\n");
+
+        printf("    6. INVENTAIRE PAR ESPECE\n");
+        printf("       -> AFFICHAGE DES ANIMAUX PAR RACE\n\n");
+
+        printf("    0. QUITTER\n");
+        printf("       -> Ferme le programme.\n\n");
+        
+        printf("============================================================\n");
+
 }
 
 int main() {
   Animal t_animaux[MAX_ANIMAL];
   int nb_animaux;
 
-  // Charger les animaux depuis le fichier au démarrage
+  // Charger les animaux depuis le fichier au demarrage
   nb_animaux = fic_to_struct(t_animaux);
 
   int choix;
-  srand(time(NULL));
+  srand(time(NULL));//fonction alea_id
 
   do {
     afficher_menu();
@@ -47,8 +67,12 @@ int main() {
       calculer_charge_nettoyage(t_animaux, nb_animaux);
       break;
 
+    case 6:
+      nb_par_espece_dec(t_animaux, nb_animaux);
+      break;
+
     case 0:
-      printf("Données enregistrées. Au revoir !\n");
+      printf("Donnees enregistrees. Au revoir !\n");
       break;
 
     default:
